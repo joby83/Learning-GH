9b0e57c186c941e26bf9e5f19e891309e2ea0a00
ByungYoon Jo <joby830801@gmail.com>
2021-07-17 12:20:02 +0900

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Byung-Yoon Jo on 2021/07/17.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
